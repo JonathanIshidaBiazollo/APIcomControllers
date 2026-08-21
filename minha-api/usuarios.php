a25caf34d2cd7a57a2a6e8a0ed854e8356c69568
JonathanIshidaBiazollo <jonathan.i.b@hotmail.com>
2026-08-21 16:30:18 -0300

    Refactor usuarios.php to improve structure and validation

--- minha-api/usuarios.php
+++ minha-api/usuarios.php
@@ -2,7 +2,9 @@
     header("Content-Type: application/json");
     require_once "conexao.php";
     require_once "funcoes.php";
+    require_once "validators/UsuarioValidator.php";
     require_once "controllers/UsuarioController.php";
+    
 
     $controller = new UsuarioController($pdo);//Já passa o $pdo aqui, pra não precisar passar toda vez que chamar um método do Controller
 
@@ -10,168 +12,43 @@
     $metodo = $_SERVER["REQUEST_METHOD"];//Informa se a requisição é GET, POST, PUT, PATCH ou DELETE
     if($metodo === "GET"){
         //BUSCAR
-        if(isset($_GET["id"])){
-            if(!is_numeric($_GET["id"])){
-                responder([
-                    "erro" => "ID inválido"
-                ], 400);
-            }
-            //Buscar um usuário em específico
-            $id = $_GET["id"];
-
-            //Sem controller
-            /*
-            $sql = "SELECT *
-                    FROM usuarios
-                    WHERE id = ?
-            ";
-
-            $stmt = $pdo->prepare($sql);
-            $stmt->execute([$id]);
-
-            $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
-            */
-
-            //Com controller
+        $id = obterIdOpcional();
+        if($id != null){
             $usuario = $controller->buscar($id);
-
             if(!$usuario){
                 responder([
                     "erro" => "Usuário não encontrado"
                 ], 404);//código de usuário não encontrado
             }
-            //echo json_encode($usuario);//Mesma coisa que a linha debaixo
+
             responder($usuario);
         }else{
-            /*
-            //Sem controller
-            $sql = "SELECT * FROM usuarios";
-            $stmt = $pdo->query($sql);
-            $usuarios = $stmt->fetchALL(PDO::FETCH_ASSOC);
-            //echo json_encode($usuarios);
-            responder($usuario);//faz a mesma coisa de cima só que usa uma função já pronta pra várias situações
-            */
             //COM CONTROLLER
-            /*
-        usuarios.php
-            │
-            ├── conexão com banco ($pdo)
-            │
-            ├── funções (responder)
-            │
-            └── UsuarioController
-                    │
-                    └── listar()
-            */
-            $usuarios = $controller->listar();//"Controller, execute o método listar e passe para ele a conexão $pdo."
+            $usuarios = $controller->listar();//"Controller, execute o método listar"
             responder($usuarios);
         }
     }else if($metodo === "POST"){
-    /*    
-    JSON
-        {
-            "nome": "Maria",
-            "email": "maria@email.com"
-        }
-    PHP
-        [
-            "nome" => "Maria",
-            "email" => "maria@email.com"
-        ]
-    */
-    //CADASTRAR
-        $dados = json_decode(//Transforma o JSON que veio pelo input do Body do POSTMAN e tranforma em um array PHP
-            file_get_contents("php://input"), //para pegar o conteúdo, que vem no BODY do POSTMAN, por exemplo
-            true
-        );
-
-        if($dados === null){
-            responder([
-                "erro" => "JSON inválido"
-            ], 400);
-        }
+        //CADASTRAR
+        $dados = obterJson();
 
         $nome = $dados["nome"] ?? "";
         $email = $dados["email"] ?? "";
 
         validarUsuario($dados);
 
-        /*
-        $sql = "INSERT INTO usuarios(
-                    nome,
-                    email)
-                VALUES(
-                    ?,
-                    ?);
-                ";
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute([
-            $nome,
-            $email
-        ]);
-        */
-
         $usuario = $controller->cadastrar($nome, $email);
-
-        /*
-        //Exemplo sem usar a função no início do código
-        http_response_code(201);//A requisição foi criada e um novo recurso foi criado
-
-        echo json_encode([
-            "mensagem" => "Usuário cadastrado com sucesso",
-            "id" => $pdo->lastInsertId(),
-            "nome" => $nome,
-            "email" => $email
-        ]);
-        */
 
         responder([
             "mensagem" => "Usuário cadastrado com sucesso",
             "usuario" => $usuario
         ], 201);
-        
-        /*
-        //Para usar no POSTMAN
-        $stmt->execute([
-            ":nome" => $nome,
-            ":email" => $email
-        ]);
-        
-        echo json_encode([
-            "mensagem" => "Usuário cadastrado com sucesso",
-            "id" => $pdo->lastInsertId()
-        ]);
-        */
+
     }else if($metodo === "PATCH"){
         //ATUALIZAR/EDITAR
-
-        //Primeiro verifico se o id não está vazio
-        if(!isset($_GET["id"])){
-            responder([
-                "erro" => "ID do usuário é obrigatório"
-            ], 400);
-        }
-
-        //Pra depois pegar o valor e atribuir a uma variável e poder usar 
-        $id = $_GET["id"];
-        if(!is_numeric($id)){
-            responder([
-                "erro" => "ID inválido"
-            ], 400);
-        }
+        $id = obterId();
 
         //Agora vou verificar se esse id existe no banco de dados pra poder editá-lo
         $usuario = $controller->buscar($id);
-
-        
-        //Antes era assim
-        /*
-        if(!$stmt->fetch()){
-            responder([
-                "erro" => "Usuário não encontrado"
-            ], 404);
-        }
-        */
 
         if(!$usuario){//$usuario vai conter os dados atuais do banco
             responder([
@@ -179,34 +56,7 @@
             ], 404);
         }
 
-        $dados = json_decode(
-            file_get_contents("php://input"),
-            true
-        );
-
-        //Faz a mesma coisa dos $dados === null
-        /*
-        if(json_last_error() !== JSON_ERROR_NONE){
-            responder([
-                "erro" => "JSON Inválido"
-            ], 400);
-        }
-
-        */
-        if($dados === null){
-            responder([
-                "erro" => "JSON Inválido"
-            ], 400);
-        }
-
-        if(!isset($dados["nome"]) && !isset($dados["email"])){
-            responder([
-                "erro" => "Informe pelo menos nome ou email"
-            ], 400);
-        }
-
-        //validarUsuario($dados);
-
+        $dados = obterJson();
 
         $nome = $dados["nome"] ?? $usuario["nome"];//se o usuário não digitar os dados utiliza os dados que o banco já possui
         $email = $dados["email"] ?? $usuario["email"];
@@ -219,85 +69,18 @@
         //email: "jonathan@email.com" -> agora pode, pq eu não enviei como vazio, só não coloquei nada, então é só usar o último registro do banco, já que no update na maioria das vezes eu não altero tudo, troco só uma coisa ou outra como email, ou endereço, mas nome e cpf por exemplo serão coisas fixas
         //É diferente do POST, pois na hora de criar um novo usuário nenhum dos campos pode ficar sem ser preenchio, já no PATCH, alguns campos podem sim ficar sem preenchimento, já que ás vezes eu só quero alterar uma coisa e pra não ter o trabalho de ficar repetindo o mesmo conteúdo que não precisa ser alterado eu faço essas validações
 
-        if(isset($dados["nome"]) && empty($dados["nome"])){//ou seja, eu verifico se o nome foi colocado, porém nunca pode estar vazio, caso contrário o banco vai salvar como vazio
-            responder([
-                "erro" => "Nome não pode ser vazio"
-            ], 400);
-        }
+        validarAtualizacaoUsuario($dados);
 
-        if(isset($dados["email"]) && empty($dados["email"])){
-            responder([
-                "erro" => "Email não pode ser vazio"
-            ], 400);
-        }
-
-        if(isset($dados["email"]) && !filter_var($dados["email"], FILTER_VALIDATE_EMAIL)){
-            responder([
-                "erro" => "Email inválido"
-            ], 400);
-        }
-
-        //Sem controllers
-        /*
-        $sql = "UPDATE usuarios
-                SET 
-                    nome = ?,
-                    email = ?
-                WHERE 
-                    id = ?
-                ";
-
-        $stmt = $pdo->prepare($sql);
-
-        $stmt->execute([
-            $nome,
-            $email,
-            $id
-        ]);
-        */
         //Com controllers
         $usuario = $controller->atualizar($id, $nome, $email);
 
-        /*
-        //Fazendo o uso do select antes para verificar se o usuário existe essa linha com rowCount não é mais necessária
-        if($stmt->rowCount() === 0){//indica qtas linhas foram afetadas, ou seja, se o id não existir, não será afetado e portanto não será dada a msg de Usuário atualizado, mas o ideal seria dar um select e depois o update, pois se vc alterar com as mesmas informações ele pode retornar que nenhuma linha foi afetada a depender do banco
-            responder([
-                "erro" => "Usuário não encontrado"
-            ], 400);
-        }
-
-        */
-        //Exemplo sem usar a função
-        /*
-        echo json_encode([
-            "mensagem" => "Usuário atualizado com sucesso",
-            "id" => $id,
-            "nome" => $nome,
-            "email" => $email
-        ]);
-        */
         responder([
             "mensagem" => "Usuário atualizado com sucesso",
             "usuario" => $usuario
         ], 200);
     }else if($metodo === "DELETE"){
         //EXCLUIR/APAGAR
-
-        //Igual no PATCH vamos verficar se mandaram um id
-        if(!isset($_GET["id"])){
-            responder([
-                "erro" => "ID do usuário é obrigatório"
-            ], 400);
-        }
-
-        //Pra depois colocar em uma variável e usá-lo
-        $id = $_GET["id"];
-
-        if(!is_numeric($id)){
-            responder([
-                "erro" => "ID inválido"
-            ], 400);
-        }
+        $id = obterId();
 
         //e depois verificar se ele existe no banco
         $usuario = $controller->buscar($id);
@@ -308,35 +91,8 @@
             ], 404);
         }
 
-        //AO invés de usar o select use o rowcount, diferente do patch ele não dá problema no delete
-        /*
-            if($stmt->rowCount() === 0){
-                responder([
-                    "erro" => "Usuário não encontrado"
-                ], 404);
-            }
-        */
-
-        //Sem controller
-        /*
-        $sql = "DELETE FROM usuarios
-                WHERE id = ?
-        ";
-
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute([
-            $id
-        ]);
-        */
         //Com controller
-        $usuario = $controller->excluir($id);
-
-        //sem usar a função responder
-        /*
-        echo json_encode([
-            "mensagem" => "Usuário deletado com sucesso"
-        ]);
-        */
+        $controller->excluir($id);
         responder([
             "mensagem" => "Usuário deletado com sucesso",
             "usuario" => $usuario
