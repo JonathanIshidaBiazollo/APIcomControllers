5beb35bb161100797f433363c567b2ab17bcc15e
JonathanIshidaBiazollo <jonathan.i.b@hotmail.com>
2026-08-21 16:31:16 -0300

    Add error handling to UsuarioController methods
    Refactor UsuarioController to handle exceptions with try-catch blocks for database operations.

--- minha-api/controllers/UsuarioController.php
+++ minha-api/controllers/UsuarioController.php
@@ -1,56 +1,5 @@
 <?php
-    //Um controller é basicamente um lugar para colocar a lógica responsável por determinada entidade.
-    //O Controller recebe uma determinada ação e coordena o que precisa acontecer.
-    /*
-        Por exemplo:
-
-        public function buscar($pdo, $id)
-        {
-            ...
-        }
-
-        Ele sabe:
-
-        "Preciso buscar um usuário."
-
-        E conversa com o banco para fazer isso.
-    */
-    //Controller não precisa necessariamente ser o responsável por tudo que envolve uma entidade.
-    /*
-        usuarios.php
-        │
-        ├── GET
-        ├── POST
-        ├── PATCH
-        └── DELETE
-
-        A ideia agora é começar a transformar isso em:
-
-        UsuarioController
-        │
-        ├── listar()
-        ├── buscar()
-        ├── cadastrar()
-        ├── atualizar()
-        └── excluir()
-    */
     class UsuarioController{
-
-        /*
-        Então o Controller passa a carregar sua própria conexão.
-
-        Por isso, quando você posteriormente faz:
-
-        $controller->buscar($id);
-
-        ele já sabe qual banco deve utilizar.
-
-        É justamente a ideia de:
-
-        "Eu entrego ao objeto aquilo que ele precisa para trabalhar."
-
-        Isso é injeção de dependência.
-        */
         private $pdo;
 
         public function __construct($pdo){//Vamos criar o construtor pra que a conexão não seja necessária de ser passada a todo momento no usuarios.php, assim vc omite essa parte
@@ -58,97 +7,118 @@
         }
 
         public function listar(){
-            $sql = "SELECT *
-                    FROM usuarios
-            ";
+            try {
+                $sql = "SELECT *
+                        FROM usuarios
+                ";
 
-            $stmt = $this->pdo->query($sql);
+                $stmt = $this->pdo->query($sql);
 
-            $usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                $usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-            return $usuarios;
+                return $usuarios;
+            } catch (PDOException $erro) {
+                erroInterno();
+            }
+
         }
 
         public function buscar($id){
-            $sql = "SELECT *
-                    FROM usuarios
-                    WHERE id = ?
-            ";
+            try{
+                $sql = "SELECT *
+                        FROM usuarios
+                        WHERE id = ?
+                ";
 
-            $stmt = $this->pdo->prepare($sql);
-            //query() → quando a SQL já está pronta:
-            //prepare() + execute() → quando você precisa colocar valores na SQL:
+                $stmt = $this->pdo->prepare($sql);
+                //query() → quando a SQL já está pronta:
+                //prepare() + execute() → quando você precisa colocar valores na SQL:
 
-            $stmt->execute([$id]);
+                $stmt->execute([$id]);
 
-            $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
+                $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
 
-            return $usuario;
+                return $usuario;                
+            }catch(PDOException $erro){
+                erroInterno();
+            }
+
         }
 
         public function cadastrar($nome, $email){
-            $sql = "INSERT INTO usuarios(
-                        nome,
-                        email
-                    )
-                    VALUES(
-                        ?,
-                        ?
-                    )
-            ";
+            try{
+                $sql = "INSERT INTO usuarios(
+                            nome,
+                            email
+                        )
+                        VALUES(
+                            ?,
+                            ?
+                        )
+                ";
 
-            $stmt = $this->pdo->prepare($sql);
+                $stmt = $this->pdo->prepare($sql);
 
-            $stmt->execute([
-                $nome,
-                $email
-            ]);
+                $stmt->execute([
+                    $nome,
+                    $email
+                ]);
 
-            return [
-                "id" => $this->pdo->lastInsertId(),
-                "nome" => $nome,
-                "email" => $email
-            ];
+                return [
+                    "id" => $this->pdo->lastInsertId(),
+                    "nome" => $nome,
+                    "email" => $email
+                ];
+            }catch(PDOException $erro){
+                erroInterno();
+            }
         }
 
         public function atualizar($id, $nome, $email){
-            $sql = "UPDATE usuarios
-                    SET
-                        nome = ?,
-                        email = ?
-                    WHERE id = ?
-            ";
+            try {
+                $sql = "UPDATE usuarios
+                        SET
+                            nome = ?,
+                            email = ?
+                        WHERE id = ?
+                ";
 
-            $stmt = $this->pdo->prepare($sql);
+                $stmt = $this->pdo->prepare($sql);
 
-            $stmt->execute([
-                $nome,
-                $email,
-                $id
-            ]);
+                $stmt->execute([
+                    $nome,
+                    $email,
+                    $id
+                ]);
 
-            return[
-                "id" => $id,
-                "nome" => $nome,
-                "email" => $email
-            ];
+                return[
+                    "id" => $id,
+                    "nome" => $nome,
+                    "email" => $email
+                ];
+            } catch (PDOException $erro) {
+                erroInterno();
+            }
+
         }
 
         public function excluir($id){
-            $sql = "DELETE
-                    FROM usuarios
-                    WHERE id = ?
-            ";
+            try {
+                $sql = "DELETE
+                        FROM usuarios
+                        WHERE id = ?
+                ";
 
-            $stmt = $this->pdo->prepare($sql);
+                $stmt = $this->pdo->prepare($sql);
 
-            $stmt->execute([
-                $id
-            ]);
+                $stmt->execute([
+                    $id
+                ]);
 
-            return[
-                "id" => $id
-            ];
+
+            } catch (PDOException $erro) {
+                erroInterno();
+            }
         }
     }
 ?>
