935121ba504df24b08db1343984632a817f5e737
JonathanIshidaBiazollo <jonathan.i.b@hotmail.com>
2026-08-21 16:29:55 -0300

    Refactor functions to include error handling and JSON parsing
    Added error handling and JSON parsing functions.

--- minha-api/funcoes.php
+++ minha-api/funcoes.php
@@ -1,20 +1,4 @@
 <?php
-    //Funções
-    //400 -> requisição inválida
-    //404 -> recurso não encontrado
-    /*
-    | Operação             | Status |
-    | -------------------- | -----: |
-    | GET encontrado       |  `200` |
-    | GET não encontrado   |  `404` |
-    | POST criado          |  `201` |
-    | PATCH atualizado     |  `200` |
-    | DELETE realizado     |  `200` |
-    | Dados inválidos      |  `400` |
-    | Método não permitido |  `405` |
-    */
-
-   
     function responder($dados, $status = 200){//se eu não passar nada fica por padrão o código 200 que significa que o status está OK
             
         http_response_code($status);
@@ -24,17 +8,24 @@
         exit;
     }
 
-    function validarUsuario($dados){
-        if(empty($dados["nome"]) || empty($dados["email"])){
+    function erroInterno(){
+        responder([
+            "erro" => "Erro interno do servidor"
+        ], 500);
+    }
+
+    function obterJson(){
+        $dados = json_decode(
+            file_get_contents("php://input"),
+            true
+        );
+
+        if($dados === null){
             responder([
-                "erro" => "Nome e email são obrigatórios"
-            ], 400);
+                "erro" => "JSON inválido"
+            ]);
         }
 
-        if(!filter_var($dados["email"], FILTER_VALIDATE_EMAIL)){//não basta colocar só o type email no html, pois o usuário pode mudar lá, tem que tratar aqui tbm
-            responder([
-                "erro" => "Email inválido"
-            ], 400);
-        }
+        return $dados;
     }
 ?>
